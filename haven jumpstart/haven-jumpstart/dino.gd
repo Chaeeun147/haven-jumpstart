4154cd89928f1bbc1cabb9db1b52f3c1d519195c
Chaeeun147 <chaeeunlim.love@gmail.com>
2026-09-21 20:55:03 -0400

    added movements to player and cactuses collision

--- haven jumpstart/haven-jumpstart/dino.gd
+++ haven jumpstart/haven-jumpstart/dino.gd
@@ -1,16 +1,21 @@
 extends CharacterBody2D
 
-const GRAVITY:  int = 6000
-const JUMP_SPEED : int = -1800
+const SPEED = 450.0
+const JUMP_VELOCITY = -550.0
 
-func _physics_process(delta):
-	velocity.y += GRAVITY * delta
-	if is_on_floor():
-		if Input.is_action_pressed("ui_accept"):
-			velocity.y = JUMP_SPEED
-		else:
-			$AnimatedSprite2D.play("running")
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
 	else:
-		$AnimatedSprite2D.play("jump")
-		
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
 	move_and_slide()
